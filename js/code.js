710a960aab57c75b03d31e6055a7a11af2435502
Rosmel - Santa Rosa <aguila097@gmail.com>
2017-10-12 12:45:59 -0500

    Comparación de fracciones terminada

--- js/code.js
+++ js/code.js
@@ -14,16 +14,14 @@ new Vue({
             var nume1 = this.numeratorOne,
                 deno1 = this.denominatorOne,
                 nume2 = this.numeratorTwo,
-                deno2 = this.denominatorTwo,
-                numeR = this.numeratorR,
-                denoR = this.denominatorR;
+                deno2 = this.denominatorTwo;
             
-            var n1 = isNaN(nume1.trim()),
-                d1 = isNaN(deno1.trim()),
-                n2 = isNaN(nume2.trim()),
-                d2 = isNaN(deno2.trim());
+            var n1 = parseInt(nume1.trim()),
+                d1 = parseInt(deno1.trim()),
+                n2 = parseInt(nume2.trim()),
+                d2 = parseInt(deno2.trim());
             
-            if(!n1 && !d1 && !n2 && !d2) {
+            if(n1 && d1 && n2 && d2) {
                 //primer caso: denominador igual 
                 if((deno1 == deno2) && (nume1 != nume2)) {
                     var numerators = [nume1, nume2],
@@ -33,6 +31,7 @@ new Vue({
                     });
                     this.numeratorR = numerators[lengthNumerators - 1];                    
                     this.denominatorR = deno1;
+                    this.comparation = 'Fracción mayor';
                 }
                 //segundo caso: numerador igual
                 if((nume1 == nume2) && (deno1 != deno2)){
@@ -43,9 +42,34 @@ new Vue({
                     });
                     this.numeratorR = nume1;                    
                     this.denominatorR = denominators[0];
+                    this.comparation = 'Fracción mayor';
+                }
+                //tercer caso: numerador y denominador diferente
+                if((nume1 != nume2) && (deno1 != deno2)) {
+                    var x = nume1 / deno1,
+                        y = nume2 / deno2,
+                        fractionMayor;
+                    var fraction = [x, y];
+                    fraction.sort(function(a, b) {
+                        return a - b;
+                    }); 
+                    fractionMayor = fraction[fraction.length - 1];
+                    if(x == y) {
+                        this.comparation = 'F. Equivalentes';
+                        this.numeratorR = nume1;                    
+                        this.denominatorR = deno1;
+                    }
+                    else if(fractionMayor == x) {
+                        this.numeratorR = nume1;                    
+                        this.denominatorR = deno1; 
+                    }
+                    else if(fractionMayor == y) {
+                        this.numeratorR = nume2;                    
+                        this.denominatorR = deno2;
+                    }
                 }
                 //fracciones iguales
-                if((nume1 == nume2) && (deno1 == deno2) && (nume1 != 0) ) {
+                if((nume1 == nume2) && (deno1 == deno2)) {
                     this.comparation = 'Fraciones iguales';
                     this.numeratorR = nume1;                    
                     this.denominatorR = deno1;
@@ -69,8 +93,3 @@ new Vue({
     }
 
 });
-/* var numbers = [4, 2, 5, 1, 3];
-numbers.sort(function(a, b) {
-  return a - b;
-});
-console.log(numbers); */
